ce726596c8c8684d4ef6897721f8e64eedfa902d
DeV <pateldev081999@gmail.com>
2024-07-03 01:52:32 -0400

    small ui change

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="blob blob3"></div>
       <div className="blob blob4"></div>
       <div className=" rounded-lg shadow-xl p-6 w-full max-w-lg glass-effect-light">
-      <h1 className="text-black text-2xl font-bold mb-4 text-center">Welcome to My Gallery App!</h1>
+      <h1 className="bg-gradient-to-r from-indigo-600 to-blue-500 bg-clip-text text-transparent text-3xl lg:text-4xl font-bold p-3 mb-4 text-center">Welcome to My Gallery App!</h1>
       <p className="mb-6 text-lg text-center text-gray-500">Sign In to Upload and save your favorite photos</p>
       <AuthForm />
       </div>
